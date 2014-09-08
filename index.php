1c6379d52e35dd9c5142bd3cad1b58c1e8b68c6d
alex-cory <alexcoryregister@gmail.com>
2014-09-08 03:55:35 -0700

    last 2 images

--- index.php
+++ index.php
@@ -193,7 +193,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-sm-3">
+                                        <!-- <div class="col-sm-3">
                                             <div class="col-item">
                                                 <div class="photo">
                                                     <img src="http://placehold.it/350x260" class="img-responsive" alt="a" />
@@ -221,8 +221,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-sm-3">
+                                        </div> -->
+                                        <!-- <div class="col-sm-3">
                                             <div class="col-item">
                                                 <div class="photo">
                                                     <img src="http://placehold.it/350x260" class="img-responsive" alt="a" />
@@ -253,7 +253,73 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </div> -->
+                                <div class="item">
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <div class="col-item">
+                                                <div class="photo">
+                                                    <img src="./img/peanut-curry.jpg" class="img-responsive" alt="a" />
+                                                </div>
+                                                <div class="info">
+                                                    <div class="row">
+                                                        <div class="price col-md-7">
+                                                            <h5 style="font-size: 10pt">Peanut Butter Chicken Curry</h5>
+                                                            <h5 class="price-text-color" style="color: #7d4702">
+                                                                $4.99</h5>
+                                                        </div>
+                                                        <div class="rating hidden-sm col-md-5">
+                                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
+                                                            </i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                                            <!-- <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
+                                                            </i><i class="price-text-color fa fa-star">
+                                                            </i> -->
+                                                        </div>
+                                                    </div>
+                                                    <div class="separator clear-left">
+                                                        <p class="btn-add">
+                                                            <i class="fa fa-shopping-cart"></i><a href="http://www.jquery2dotnet.com" class="hidden-sm" >Add to cart</a></p>
+                                                        <p class="btn-details">
+                                                            <i class="fa fa-list"></i><a href="http://www.jquery2dotnet.com" class="hidden-sm">More details</a></p>
+                                                    </div>
+                                                    <div class="clearfix">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                <div class="item">
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <div class="col-item">
+                                                <div class="photo">
+                                                    <img src="./img/peanut-curry.jpg" class="img-responsive" alt="a" />
+                                                </div>
+                                                <div class="info">
+                                                    <div class="row">
+                                                        <div class="price col-md-7">
+                                                            <h5 style="font-size: 10pt">Peanut Butter Chicken Curry</h5>
+                                                            <h5 class="price-text-color" style="color: #7d4702">
+                                                                $4.99</h5>
+                                                        </div>
+                                                        <div class="rating hidden-sm col-md-5">
+                                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
+                                                            </i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                                            <!-- <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
+                                                            </i><i class="price-text-color fa fa-star">
+                                                            </i> -->
+                                                        </div>
+                                                    </div>
+                                                    <div class="separator clear-left">
+                                                        <p class="btn-add">
+                                                            <i class="fa fa-shopping-cart"></i><a href="http://www.jquery2dotnet.com" class="hidden-sm" >Add to cart</a></p>
+                                                        <p class="btn-details">
+                                                            <i class="fa fa-list"></i><a href="http://www.jquery2dotnet.com" class="hidden-sm">More details</a></p>
+                                                    </div>
+                                                    <div class="clearfix">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                 <div class="item">
                                     <div class="row">
                                         <div class="col-sm-3">
